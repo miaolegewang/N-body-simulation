cfeb5bb2350a2eadadc01541bfefe964459f9116
Chen Lai <littlelittlecc@gmail.com>
2016-03-11 16:13:09 -0800

    play

--- p_v_generator.m
+++ p_v_generator.m
@@ -1,12 +1,15 @@
 G = 0.287915013;
-M = 19.5;
+M1 = 17;    %Mass of Milkway. You can modify it
+M2 = 36;    %Mass of Andromeda. You can modify it  
 distance = 780;
 
-radius1 = [-369.8+8.5, 615.4 -304.8];  %distance between milkway and andromeda
-radius2 = [-8.5,0,0];
-center = (radius1 + radius2)/2;
-c_1 = (radius1 - center)/25;
-c_2 = (radius2 - center)/25;
+radius1 = [-369.8+8.5, 615.4 -304.8]/25;  %Ralative position of andromeda. You can modify it. 
+                                          %But please comment where the
+                                          %data comes from
+radius2 = [0,0,0]/25;                     %When position of the Milkway is (0,0,0). Don't modify it.                                        
+center = (radius1)/(M1 + M2);               
+c_1 = -M2 * center;
+c_2 = (-M1/M2)*c_1;
 
 normp = sqrt((369.8-8.5)^2 + (615.4)^2 + (304.8)^2);
 
@@ -15,10 +18,10 @@ n1 = (369.8-8.5)/normp;
 n2 = -615.4/normp;
 n3 = 304.8/normp;
 
-translate = 0.00408804856;
+translate = 0.00408804856;  %unit km/s to 25 kpc / 10^8 year. Don't modify it
 
-vr_mag = 117;     %radius velocity
-vt_mag = 40;      %tranverse velocity
+vr_mag = 117;     %radius velocity. Feel free to modify it !!!
+vt_mag = 40;      %tranverse velocity. Feel free to modify it !!!
 vr = vr_mag * [n1, n2, n3];
 
 v1 = vr;
@@ -45,7 +48,11 @@ time = 2 * pi * (780/25)^2 /sqrt(0.28*78)/20000;
 
 v_absolute = vsum_kpcy/2;
 
-p_milkway = c_1;
-v_milkway = -v_absolute;
-p_andromeda = c_2;
-v_andromeda = v_absolute;
+centerv = v_absolute/(M1 + M2);
+v_1 = -M2 * centerv;
+v_2 = (-M1/M2)*v_1;
+
+p_milkway = c_1;    %position of milkway(x,y,z)
+v_milkway = v_1;    %velocity of milkway(vx,vy,vx)
+p_andromeda = c_2;  %position of milkway(x,y,z)
+v_andromeda = v_2;  %velocity of milkway(vx,vy,vz)
