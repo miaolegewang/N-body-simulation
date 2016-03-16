939ba8fb3be7c4a84ed96c95c0f4e395836c57a9
Zhongrong <jiansmith.zj@gmail.com>
2016-03-16 14:26:37 -0700

    Functions rearrange

--- andromeda.cu
+++ andromeda.cu
@@ -24,86 +24,9 @@
 #include <curand.h>
 #include <curand_kernel.h>
 #include "dataDeclaration.h"
+#include "functionDeclaration.h"
 #include "otherfunctions.c"
 
-
-/*
- *  Major Function Declarations Section
- *
- */
-
-__global__ void leapstep(int, double*, double*, double*, double*, double*, double*, double);
-/*
- *  leapstep - kernel function: update positions using leapfrog algorithm
- *    parameters:
- *      [#particles] [x position] [y position] [x position]
- *                   [x velocity] [y velocity] [z velocity] [dt]
- *
- */
-__global__ void accel(int, double*, double*, double*, double*, double*, double*, double*, double);
-/*
- *  accel - kernel function: update velocity using leapfrog algorithm
- *    parameters:
- *      [#particles] [x position] [y position] [x position]
- *                   [x velocity] [y velocity] [z velocity]
- *                   [mass] [dt]
- *
- */
-
-__global__ void printstate(int, double*, double*, double*, double*, double*, double*, int);
-/*
- *  printstate - kernel function: print position and velocity
- *    parameters:
- *      [#particles] [x position] [y position] [x position]
- *                   [x velocity] [y velocity] [z velocity] [tnow]
- *
- */
-
-void initialCondition_host_file(char *, char *, double **, double **, double **, double **, double **, double **, double **, int *);
-/*
- *  initialCondition_host_file - host function: create both galaxies by extracting data from files
- *    parameters:
- *      [filename galaxy 1] [filename galaxy 2]
- *                 [x position array addr] [y position array addr] [z position array addr]
- *                 [x velocity array addr] [y velocity array addr] [z velocity array addr]
- *                 [mass array addr] [size addr]
- *
- */
-void create_galaxy_file(FILE *, double*, double*, double*, double*, double*, double*, double*);
-/*
- *  create_galaxy_file - host function: create galaxy by reading data from file
- *    parameters:
- *      [filename galaxy] [x position] [y position] [z position]
- *                        [x velocity] [y velocity] [z velocity]
- *                        [mass]
- *
- */
-void initialCondition_host_file(char *, char *, double **, double **, double **, double **, double **, double **, double **, int *);
-/*
- *  initialCondition_host_file - host function: create both galaxies by extracting data from files
- *    parameters:
- *      [filename galaxy 1] [filename galaxy 2]
- *                 [x position array addr] [y position array addr] [z position array addr]
- *                 [x velocity array addr] [y velocity array addr] [z velocity array addr]
- *                 [mass array addr] [size addr]
- *
- */
-void create_galaxy_file(FILE *, double*, double*, double*, double*, double*, double*, double*);
-/*
- *  create_galaxy_file - host function: create galaxy by reading data from file
- *    parameters:
- *      [filename galaxy] [x position] [y position] [z position]
- *                        [x velocity] [y velocity] [z velocity]
- *                        [mass]
- *
- */
-
-/*
- *  Helper Function Declarations
- *
- */
-void rotate(double*, double*, double*, double, double, double, double);
-void read_size_from_file(FILE*, int*, double*);
 
 /**     Main function     **/
 int main(int argc, char *argv[])
