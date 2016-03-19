3c11de46182f63b638e235e63491e684b76ca1fd
Juan Sidrach de Cardona Mora <jsidrach@ucsd.edu>
2016-03-18 18:50:43 -0700

    First batch of cleanup

--- andromeda_nb.cu
+++ andromeda_nb.cu
@@ -15,7 +15,6 @@
  *	  *7. print mass[i]. because the halo is dark matter. Or better way distinguish dark matter and rings?
  */
 
-
 #include <cuda.h>
 #include <math.h>
 #include <string.h>
@@ -23,14 +22,36 @@
 #include <stdlib.h>
 #include <curand.h>
 #include <curand_kernel.h>
-#include "dataDeclaration.h"
-#include "functionDeclaration.h"
-#include "otherfunctions.c"
 
+#define PI 3.14159265
+#define BUFFERSIZE 500
+#define BLOCKSIZE 256
+#define G 1.0
+#define MASS_1 38.2352941              // Center mass of Milky Way
+#define SOFTPARAMETER 0.000001
+#define AndromedaXOffsetP -41.0882
+#define AndromedaYOffsetP 68.3823
+#define AndromedaZOffsetP -33.8634
+#define AndromedaXOffsetV 0.0420
+#define AndromedaYOffsetV -0.2504
+#define AndromedaZOffsetV 0.1240
+#define MilkwayXOffsetP 41.0882
+#define MilkwayYOffsetP -68.3823
+#define MilkwayZOffsetP 33.8634
+#define MilkwayXOffsetV -0.0420
+#define MilkwayYOffsetV 0.2504
+#define MilkwayZOffsetV -0.1240
+
+// Headers
+void rotate(double* x, double* y, double *z, double n1, double n2, double n3, double theta);
+__global__ void leapstep(unsigned int n, double *x, double *y, double *z, double *vx, double *vy, double *vz, double dt);
+__global__ void accel(unsigned int n, double *x, double *y, double *z, double *vx, double *vy, double *vz, double* mass, double dt);
+__global__ void printstate(double *x, double *y, double *z, unsigned int tnow);
+void initialCondition_host_file(char *input1, char *input2, double **x, double **y, double **z, double **vx, double **vy, double **vz, double **mass, unsigned int *size);
+void read_size_from_file(char *input, unsigned int *size) ;
 
 /**     Main function     **/
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   /*
    *  Handling commandline inputs and setting initial value of the arguments
    *    1. number of steps (mstep)
@@ -39,15 +60,18 @@ int main(int argc, char *argv[])
    *    4. timestamp (dt)
    *
    */
-  int mstep, nout, offset, tnow = 0, n;
+  unsigned int mstep, nout, offset, tnow = 0, n;
   double dt, *x, *y, *z, *vx, *vy, *vz, *mass;
-  mstep = argc > 1 ? atoi(argv[1]) : 100;
-  nout = argc > 2 ? atoi(argv[2]) : 1;
-  offset = argc > 3 ? atoi(argv[3]) : 0;
-  dt = argc > 4 ? atof(argv[4]) : 2 * PI * RMIN * RMIN /sqrt(G * MASS_1) / 40.0;
-//   dt = argc > 4 ? atof(argv[4]) : 0.1;
+
+  mstep = (argc > 1) ? atoi(argv[1]) : 100;
+  nout = (argc > 2) ? atoi(argv[2]) : 1;
+  offset = (argc > 3) ? atoi(argv[3]) : 0;
+  dt = (argc > 4) ? atof(argv[4]) : (2.0 * PI * RMIN * RMIN) / (sqrt(G * MASS_1) * 40.0);
+
   initialCondition_host_file("milky_way.dat", "andromeda.dat", &x, &y, &z, &vx, &vy, &vz, &mass, &n);
-  int grids = ceil((double)n / BLOCKSIZE), threads = BLOCKSIZE;
+
+  unsigned int grids = ceil((double)n / BLOCKSIZE), threads = BLOCKSIZE;
+
   /*
    *  Use cudaDeviceSetLimit() to change the buffer size of printf
    *   used in kernel functions to solve the problem encountered before:
@@ -55,144 +79,137 @@ int main(int argc, char *argv[])
    *
    */
   cudaDeviceSetLimit(cudaLimitPrintfFifoSize, n * BUFFERSIZE);
+
   /*  Start looping steps from first step to mstep  */
-  for(int i = 0; i < offset; i++, tnow++){
-    accel<<< grids, threads >>>(n, x, y, z, vx, vy, vz, mass, dt);
+  for (unsigned int i = 0; i < offset; i++, tnow++){
+    accel<<<grids, BLOCKSIZE>>> (n, x, y, z, vx, vy, vz, mass, dt);
     cudaDeviceSynchronize();
-    leapstep<<< grids, threads >>>(n, x, y, z, vx, vy, vz, dt);
+    leapstep<<<grids, BLOCKSIZE>>> (n, x, y, z, vx, vy, vz, dt);
     cudaDeviceSynchronize();
-    accel<<< grids, threads >>>(n, x, y, z, vx, vy, vz, mass, dt);
-    cudaDeviceSynchronize();
-    //counter++;
-  }
-  for(int i = offset; i < mstep; i++, tnow++){
-    if(i % nout == 0)
-      printstate<<< grids, threads >>>(n, x, y, z, vx, vy, vz, tnow);
-    cudaDeviceSynchronize();
-    /*
-     *  Use cudaDeviceSynchronize() to wait till all blocks of threads
-     *   finish running the kernel functions
-     *  Since between each accel() is called, the position of each particle
-     *   is updated, which affect the second accel() calls, we need sychronize
-     *   in the middle
-     *
-     */
-    accel<<< grids, threads >>>(n, x, y, z, vx, vy, vz, mass, dt);
-    cudaDeviceSynchronize();
-    leapstep<<< grids, threads >>>(n, x, y, z, vx, vy, vz, dt);
-    cudaDeviceSynchronize();
-    accel<<< grids, threads >>>(n, x, y, z, vx, vy, vz, mass, dt);
+    accel<<<grids, BLOCKSIZE>>> (n, x, y, z, vx, vy, vz, mass, dt);
     cudaDeviceSynchronize();
   }
-  if(mstep % nout == 0)
-    printstate<<< grids, threads >>>(n, x, y, z, vx, vy, vz, tnow);
+  for (unsigned int i = offset; i < mstep; i++, tnow++) {
+    if(i % nout == 0) {
+      printstate<<<grids, threads>>> (n, x, y, z, vx, vy, vz, tnow);
+      cudaDeviceSynchronize();
+    }
+
+    accel<<<grids, BLOCKSIZE>>> (n, x, y, z, vx, vy, vz, mass, dt);
+    cudaDeviceSynchronize();
+    leapstep<<<grids, BLOCKSIZE>>> (n, x, y, z, vx, vy, vz, dt);
+    cudaDeviceSynchronize();
+    accel<<<grids, BLOCKSIZE>>> (n, x, y, z, vx, vy, vz, mass, dt);
+    cudaDeviceSynchronize();
+  }
+
+  if(mstep % nout == 0) {
+    printstate<<<grids, BLOCKSIZE>>>(n, x, y, z, vx, vy, vz, tnow);
+  }
   cudaDeviceSynchronize();
 
   // After finishing, free the allocated memory
   cudaFree(x);
-  cudaFree(y);
-  cudaFree(z);
-  cudaFree(vx);
-  cudaFree(vy);
-  cudaFree(vz);
-  cudaFree(mass);
 
   // Exit the current thread
   return 0;
 }
 
-/*
- *  Functions Implmenetation Section
- *
- */
-__global__ void initialConditions(int n, double* x, double* y, double* z, double* vx, double* vy, double* vz, double* mass){
-  /*  TODO    */
-}
+void rotate(double* x, double* y, double *z, double n1, double n2, double n3, double theta) {
+  double sigma = -theta;
+  double c = cos(sigma);
+  double s = sin(sigma);
+  double a = 1 - cos(sigma);
 
-void rotate(double* x, double* y, double *z, double n1, double n2, double n3, double theta){
-
-   double tmpx, tmpy, tmpz;
-   double a, c, s, sigma;
-
-   sigma = -theta;
-   c = cos(sigma);
-   s = sin(sigma);
-   a = 1 - cos(sigma);
-
-
-  tmpx = ( a * n1 * n1 + c ) * (*x) + ( a * n1 * n2 - s * n3) * (*y) + ( a * n1 * n3 + s * n2 ) * (*z);
-  tmpy = ( a * n1 * n2 + s * n3) * (*x) + ( a * n2 * n2 + c) * (*y) + ( a * n2 * n3 - s * n1 ) * (*z);
-  tmpz = ( a * n1 * n3 - s * n2) * (*x) + ( a * n2 * n3 + s * n1) * (*y) + ( a * n3 * n3 + c) * (*z);
+  double tmpx = ( a * n1 * n1 + c ) * (*x) + ( a * n1 * n2 - s * n3 ) * (*y) + ( a * n1 * n3 + s * n2 ) * (*z);
+  double tmpy = ( a * n1 * n2 + s * n3 ) * (*x) + ( a * n2 * n2 + c ) * (*y) + ( a * n2 * n3 - s * n1 ) * (*z);
+  double tmpz = ( a * n1 * n3 - s * n2 ) * (*x) + ( a * n2 * n3 + s * n1 ) * (*y) + ( a * n3 * n3 + c ) * (*z);
 
   (*x) = tmpx;
   (*y) = tmpy;
   (*z) = tmpz;
-
 }
 
-__global__ void leapstep(int n, double *x, double *y, double *z, double *vx, double *vy, double *vz, double dt){
-  const unsigned int serial = blockIdx.x * BLOCKSIZE + threadIdx.x;
-  if(serial < n){
+__global__ void leapstep(unsigned int n, double *x, double *y, double *z, double *vx, double *vy, double *vz, double dt) {
+  const unsigned int serial = blockIdx.x * blockDim.x + threadIdx.x;
+  if (serial < n){
     x[serial] += dt * vx[serial];
     y[serial] += dt * vy[serial];
     z[serial] += dt * vz[serial];
   }
 }
 
-
-__global__ void accel(int n, double *x, double *y, double *z, double *vx, double *vy, double *vz, double* mass, double dt){
-  const unsigned int serial = blockIdx.x * BLOCKSIZE + threadIdx.x;
+__global__ void accel(unsigned int n, double *x, double *y, double *z, double *vx, double *vy, double *vz, double* mass, double dt) {
+  const unsigned int serial = blockIdx.x * blockDim.x + threadIdx.x;
   const unsigned int tdx = threadIdx.x;
+
   __shared__ double lx[BLOCKSIZE];
   __shared__ double ly[BLOCKSIZE];
   __shared__ double lz[BLOCKSIZE];
   __shared__ double lm[BLOCKSIZE];
-  double ax = 0.0, ay = 0.0, az = 0.0, norm, thisX = x[serial], thisY = y[serial], thisZ = z[serial];
-  for(int i = 0; i < gridDim.x; i++){
-    // Copy data from main memory
-    lx[tdx] = x[i * BLOCKSIZE + tdx];
-    lz[tdx] = y[i * BLOCKSIZE + tdx];
-    ly[tdx] = z[i * BLOCKSIZE + tdx];
-    lm[tdx] = mass[i * BLOCKSIZE + tdx];
+
+  double ax = 0.0, ay = 0.0, az = 0.0;
+  double norm;
+  double thisX, thisY, thisZ;
+  if (serial < n) {
+    thisX = x[serial];
+    thisY = y[serial];
+    thisZ = z[serial];
+  }
+
+  for (unsigned int i = 0; i < gridDim.x; i++) {
+    unsigned long index = i * blockDim.x + tdx;
+    if (index < n) {
+      // Copy data from main memory
+      lx[tdx] = x[index];
+      lz[tdx] = y[index];
+      ly[tdx] = z[index];
+      lm[tdx] = mass[index];
+    }
     __syncthreads();
+
     // Accumulates the acceleration
     #pragma unroll
-    for(int j = 0; j < BLOCKSIZE;j++){
-      // loop unrolling
+    for (unsigned int j = 0; j < BLOCKSIZE; j++) {
+      unsigned long pos = i * blockDim.x + j;
+      if (pos >= n) {
+        continue;
+      }
+
       norm = pow(SOFTPARAMETER + pow(thisX - lx[j], 2) + pow(thisY - ly[j], 2) + pow(thisZ - lz[j], 2), 1.5);
       ax += - G * lm[j] * (thisX - lx[j]) / norm;
       ay += - G * lm[j] * (thisY - ly[j]) / norm;
       az += - G * lm[j] * (thisZ - lz[j]) / norm;
     }
+
     __syncthreads();
   }
-  if(serial < n){
-    //printf("%d\n", serial);
+
+  if (serial < n) {
     vx[serial] += 0.5 * dt * ax;
     vy[serial] += 0.5 * dt * ay;
     vz[serial] += 0.5 * dt * az;
   }
 }
 
-__global__ void printstate(int n, double *x, double *y, double *z, double *vx, double *vy, double *vz, int tnow){
+__global__ void printstate(double *x, double *y, double *z, unsigned int tnow) {
   const unsigned int serial = blockIdx.x * blockDim.x + threadIdx.x;
-  if(serial < 10000 || (serial > 44000 && serial < 54001)){
-    printf("%d, %12.6f, %12.6f, %12.6f, %12.6f, %12.6f, %12.6f, %d\n", serial, x[serial], y[serial], z[serial], vx[serial], vy[serial], vz[serial], tnow);
+  if(serial < 10000 || (serial >= 44000 && serial < 54000)){
+    printf("%d,%12.6lf,%12.6lf,%12.6lf,%d\n", serial, x[serial], y[serial], z[serial], tnow);
   }
 }
 
-void initialCondition_host_file(char *input1, char *input2, double **x, double **y, double **z, double **vx, double **vy, double **vz, double **mass, int *size){
-  int s1, s2;
-  double unknown;
+void initialCondition_host_file(char *input1, char *input2, double **x, double **y, double **z, double **vx, double **vy, double **vz, double **mass, unsigned int *size) {
+  unsigned int s1, s2;
   read_size_from_file(input1, &s1);
   (*size) = s1;
   read_size_from_file(input2, &s2);
   (*size) += s2;
-  s1 = (*size) - s2;
-  int numOfBlocks = ceil((double)(*size) / BLOCKSIZE);
+  unsigned int numOfBlocks = ceil(((double)(*size)) / BLOCKSIZE);
+
   // Initial local data array
   double *lx, *ly, *lz, *lvx, *lvy, *lvz, *lm;
-  lx = (double*)malloc(numOfBlocks * BLOCKSIZE * 7 * sizeof(double));
+  lx = (double*) malloc(7 * numOfBlocks * BLOCKSIZE * sizeof(double));
   ly = lx + numOfBlocks * BLOCKSIZE;
   lz = ly + numOfBlocks * BLOCKSIZE;
   lvx = lz + numOfBlocks * BLOCKSIZE;
@@ -206,13 +223,18 @@ void initialCondition_host_file(char *input1, char *input2, double **x, double *
     printf("Error: fail to open file 1\n");
     exit(-1);
   }
-  int junk1;
+  unsigned int count = 0;
+
+  // Skip first galaxy
+  unsigned long junk1;
   double junk2;
-  int count = 0;
-  fscanf(fp, "%lu %lf", &junk1, &junk2);    // skip first line
-  double omega = 0.0, sigma = PI / 2.0;
-  while(!feof(fp) && count < s1){
-    fscanf(fp, "%lf %lf %lf %lf %lf %lf %lf", lm + count, lx + count, ly + count, lz + count, lvx + count, lvy + count, lvz + count);
+  fscanf(fp, "%lu %lf\n", &junk1, &junk2);
+
+  double omega = 0.0;
+  double sigma = PI / 2.0;
+
+  while((!feof(fp)) && (count < s1)){
+    fscanf(fp, "%lf %lf %lf %lf %lf %lf %lf\n", lm + count, lx + count, ly + count, lz + count, lvx + count, lvy + count, lvz + count);
     rotate(lx + count, ly + count, lz + count, cos(omega), sin(omega), 0, sigma);
     rotate(lvx + count, lvy + count, lvz + count, cos(omega), sin(omega), 0, sigma);
     *(lx + count) += MilkwayXOffsetP;
@@ -231,11 +253,15 @@ void initialCondition_host_file(char *input1, char *input2, double **x, double *
     printf("Error: fail to open file 2\n");
     exit(-1);
   }
-  fscanf(fp, "%lu %lf", &junk1, &junk2);    // skip first line
-  omega = -2 * PI / 3;
-  sigma = PI / 6;
-  while(!feof(fp) && count < (*size)){
-    fscanf(fp, "%lf %lf %lf %lf %lf %lf %lf", lm + count, lx + count, ly + count, lz + count, lvx + count, lvy + count, lvz + count);
+
+  // Skip first line
+  fscanf(fp, "%lu %lf\n", &junk1, &junk2);
+
+  omega = - 2.0 * PI / 3.0;
+  sigma = PI / 6.0;
+
+  while((feof(fp)) && (count < (*size))){
+    fscanf(fp, "%lf %lf %lf %lf %lf %lf %lf\n", lm + count, lx + count, ly + count, lz + count, lvx + count, lvy + count, lvz + count);
     rotate(lx + count, ly + count, lz + count, cos(omega), sin(omega), 0, sigma);
     rotate(lvx + count, lvy + count, lvz + count, cos(omega), sin(omega), 0, sigma);
     *(lx + count) += AndromedaXOffsetP;
@@ -247,36 +273,21 @@ void initialCondition_host_file(char *input1, char *input2, double **x, double *
     count++;
   }
   fclose(fp);
-  size_t extra = numOfBlocks * BLOCKSIZE - (*size);
-  memset(lx + count, 0, extra * sizeof(double));
-  memset(ly + count, 0, extra * sizeof(double));
-  memset(lz + count, 0, extra * sizeof(double));
-  memset(lvx + count, 0, extra * sizeof(double));
-  memset(lvy + count, 0, extra * sizeof(double));
-  memset(lvz + count, 0, extra * sizeof(double));
-  memset(lm + count, 0, extra * sizeof(double));
+
   // Allocate device memory
-  cudaMalloc((void**)x, numOfBlocks * BLOCKSIZE * 7 * sizeof(double));
-  *y = *x + numOfBlocks * BLOCKSIZE;
-  *z = *y + numOfBlocks * BLOCKSIZE;
-  *vx = *z + numOfBlocks * BLOCKSIZE;
-  *vy = *vx + numOfBlocks * BLOCKSIZE;
-  *vz = *vy + numOfBlocks * BLOCKSIZE;
-  *mass = *vz + numOfBlocks * BLOCKSIZE;
-  cudaMemcpy(*x, lx, (size_t)numOfBlocks * BLOCKSIZE * sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(*y, ly, (size_t)numOfBlocks * BLOCKSIZE * sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(*z, lz, (size_t)numOfBlocks * BLOCKSIZE * sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(*vx, lvx, (size_t)numOfBlocks * BLOCKSIZE * sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(*vy, lvy, (size_t)numOfBlocks * BLOCKSIZE * sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(*vz, lvz, (size_t)numOfBlocks * BLOCKSIZE * sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(*mass, lm, (size_t)numOfBlocks * BLOCKSIZE * sizeof(double), cudaMemcpyHostToDevice);
+  cudaMalloc(x, 7 * numOfBlocks * BLOCKSIZE * sizeof(double));
+  (*y) = (*x) + numOfBlocks * BLOCKSIZE;
+  (*z) = (*y) + numOfBlocks * BLOCKSIZE;
+  (*vx) = (*z) + numOfBlocks * BLOCKSIZE;
+  (*vy) = (*vx) + numOfBlocks * BLOCKSIZE;
+  (*vz) = (*vy) + numOfBlocks * BLOCKSIZE;
+  (*mass) = (*vz) + numOfBlocks * BLOCKSIZE;
+  cudaMemcpy((*x), lx, 7 * numOfBlocks * BLOCKSIZE * sizeof(double), cudaMemcpyHostToDevice);
   free(lx);
 }
 
-void read_size_from_file(char *input, int *size){
+void read_size_from_file(char *input, unsigned int *size) {
   FILE *fp = fopen(input, "r");
-  double unknown;
   fscanf(fp, "%lu", size);
-  (*size)++;
   fclose(fp);
 }
